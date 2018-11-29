96f22dfadfc8cfb72899b5df59b52fc800b77b4e
Matheus Castello <matheus@castello.eng.br>
2018-11-29 00:44:32 -0200

    shell: fix gpio2018 to gpio24
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- blink.sh
+++ blink.sh
@@ -5,8 +5,8 @@ echo out > /sys/class/gpio/gpio2018/direction
 
 while [ 1 ]
 do
-	echo 1 > /sys/class/gpio/gpio2018/value
+	echo 1 > /sys/class/gpio/gpio24/value
 	sleep 0.2s
-	echo 0 > /sys/class/gpio/gpio2018/value
+	echo 0 > /sys/class/gpio/gpio24/value
 	sleep 0.2s
 done
