0d15266f6bdad8d196d838c64e376fc363f48e4b
Matheus Castello <matheus@castello.eng.br>
2018-11-29 01:15:01 -0200

    shell: unit: add shutdown
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- blinkShutdown.sh
+++ blinkShutdown.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i {1..5}
+for (( c=1; c<=5; c++ ))
 do
-	echo 1 > /sys/class/gpio/gpio24/value
-	sleep 0.09s
-	echo 0 > /sys/class/gpio/gpio24/value
-	sleep 0.09s
+        echo 1 > /sys/class/gpio/gpio24/value
+        sleep 0.09s
+        echo 0 > /sys/class/gpio/gpio24/value
+        sleep 0.09s
 done
 
 echo 24 > /sys/class/gpio/unexport
